aa415976a81046d74ea8519a407129dc73c1b5d0
GabrielWesz <54816775+GabrielWesz@users.noreply.github.com>
2020-03-15 14:20:03 -0300

    Add files via upload

---
+++ grafo.py
@@ -0,0 +1,51 @@
+class Grafo():
+    def __init__(self):
+        pass
+
+    def le_grafo(self, arq):
+        """
+        Implementa a funcionalidade de leitura de um grafo, passado como um arquivo
+        """
+        pass
+
+    def peso(self, u, v):
+        """
+        Retorna o peso de uma aresta do grafo
+        """
+        pass
+
+    def ha_arestas(self, u, v):
+        """
+        Confere se existe aresta entre u e v
+        """
+        pass
+
+    def vizinhos(self, v):
+        """
+        Retorna os vizinhos do vertice v
+        """
+        pass
+
+    def rotulo(self, v):
+        """
+        Retorna o rotulo do vertice v
+        """
+        pass
+
+    def grau(self, v):
+        """
+        Retorna o grau do vertice v
+        """
+        pass
+
+    def qnt_arestas(self):
+        """
+        Retorna a quantidade de arestas
+        """
+        pass
+
+    def qnt_vertices(self):
+        """
+        Retorna a quantidade de vertices
+        """
+        pass
