803ec98f0911d71a10f99c4dbac70e0611b1969a
GabrielWesz <54816775+GabrielWesz@users.noreply.github.com>
2020-03-15 15:50:54 -0300

    Add files via upload

--- grafo.py
+++ grafo.py
@@ -1,12 +1,36 @@
-class Grafo():
-    def __init__(self):
-        pass
+class Grafo:
+    def __init__(self, file, n_vertices=0, n_arestas=0):
+        self.file = file
+        self.n_vertices = n_vertices
+        self.n_arestas = n_arestas
+        self.v = list()
+        self.e = list()
 
-    def le_grafo(self, arq):
+    def le_grafo(self):
         """
-        Implementa a funcionalidade de leitura de um grafo, passado como um arquivo
+        Implementa a funcionalidade de leitura de um grafo
+        Abre o arquivo file, contido pelo grafo que chamou este metodo
         """
-        pass
+        arq = open(self.file, 'r')
+        linha1 = arq.readline().split()
+        vertice = dict()
+        self.n_vertices = int(linha1[1])
+        for i in range(0, self.n_vertices):
+            l = arq.readline().split()
+            vertice["Indice"] = l[0]
+            vertice["Rotulo"] = ' '.join(l[1:])
+            self.v.append(vertice.copy())
+            vertice.clear()
+        arq.readline()  # Serve para pular a linha em que está escrito '*edges'
+        aresta = dict()
+        for l in arq:
+            linha = l.split()
+            aresta["V1"] = linha[0]
+            aresta["V2"] = linha[1]
+            aresta["Peso"] = linha[2]
+            self.e.append(aresta.copy())
+            aresta.clear()
+        self.n_arestas = len(self.e)
 
     def peso(self, u, v):
         """
@@ -40,12 +64,12 @@ class Grafo():
 
     def qnt_arestas(self):
         """
-        Retorna a quantidade de arestas
+        Retorna a quantidade de arestas do grafo
         """
-        pass
+        return self.n_arestas
 
     def qnt_vertices(self):
         """
-        Retorna a quantidade de vertices
+        Retorna a quantidade de vertices do grafo
         """
-        pass
+        return self.n_vertices
--- testes.py
+++ testes.py
@@ -1,3 +1,8 @@
 from grafo import Grafo
 
-g1 = Grafo()
+file = input("Digite nome do arquivo: ")
+g1 = Grafo(file)
+g1.le_grafo()
+print(g1.qnt_arestas())
+print(g1.qnt_vertices())
+
