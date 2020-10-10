859cd9125e921a0246144a63a4bf2ace1e0d3db0
GabrielWesz <54816775+GabrielWesz@users.noreply.github.com>
2020-10-10 17:23:49 -0300

    Update ciclo_euleriano.py

--- ciclo_euleriano.py
+++ ciclo_euleriano.py
@@ -1,10 +1,6 @@
-from grafo import Grafo
-from queue import Queue
-
-
 class Ciclo:
 
-    def __init__(self, arq_grafo, index_origem):
+    def __init__(self, arq_grafo):
         """
         Recebe um grafo já lido pelo método grafo.le_grafo()
         Cria lista para indicar se uma aresta já foi visitada (list_visitado)
@@ -14,71 +10,33 @@ class Ciclo:
         self.list_visitado = list()
         self.list_ciclo = list()
         self.lista_vertices = self.g1.adj_list
-        self.index_origem = index_origem
 
-        """
-        Popula a list_visitado, que possuem tamanho igual ao número
-        de arestas do grafo
-        Obs: a list_ciclo não precisa ser populada agora
-        """
 
-        for v in range(len(self.lista_vertices)):
-            self.list_visitado.append(list())
-            for e in range(len(self.lista_vertices[v])):
-                self.list_visitado[v].append(False)
+    def caminho_euleriano(self):
 
-    """
-    Implementa o algoritmo de Hierholzer, descrito na página 35 das anotações da disciplina
-    Esse algoritmo invoca um algoritmo auxiliar, o alg_subciclo()
+        pilha_vertices = [3]
 
-    """
+        caminho = list()
 
-    def alg_hierholzer(self):
+        while pilha_vertices:
+            vertice_atual = pilha_vertices[-1]
 
-        ciclo = self.alg_subciclo(self.index_origem)
-        if ciclo == None:
-            return None
-        else:
-            for v in self.list_visitado:
-                if False in v:
-                    return None
-            self.list_ciclo.append(ciclo)
-            return ciclo
+            if self.lista_vertices[vertice_atual - 1]:
+                proximo_vertice = self.lista_vertices[vertice_atual - 1][0]["vertice"]
+                del self.lista_vertices[vertice_atual - 1][0]
 
-    """
-    Implementa o algoritmo auxiliar de busca de subciclos eulerianos, descrito na página 36 das anotações
-    É invocado pela função alg_hierholzer()
-    """
+                pilha_vertices.append(proximo_vertice)
 
-    def alg_subciclo(self, v_atual):
-        ciclo = list()
-        ciclo.append(v_atual)
-        t = v_atual
-        t_equals_v = False
-
-        while not t_equals_v:
-
-            if not False in self.list_visitado[v_atual - 1]:
-                return None
             else:
-                u = 0
-                while self.list_visitado[v_atual - 1][u]:
-                    u = u + 1
-                else:
-                    vertice = self.lista_vertices[v_atual - 1][u]["vertice"]
-                    self.list_visitado[v_atual - 1][u] = True
-                    v_atual = vertice
-                    ciclo.append(vertice)
-            t_equals_v = t == v_atual
+                caminho.append(pilha_vertices.pop())
 
-        for vertice in range(len(self.list_visitado)):
-            for e in range(len(self.list_visitado[vertice])):
-                if not self.list_visitado[vertice][e]:
-                    subciclo = self.alg_subciclo(self.lista_vertices[vertice][e]["vertice"])
-                    if subciclo is None:
-                        return None
-                    ciclo = self.adiciona_subciclo(ciclo, subciclo)
-        return ciclo
+        return caminho
 
-    def adiciona_subciclo(self, ciclo, subciclo):
-        return 0
+
+    def ciclo_euleriano(self):
+        ciclo = self.caminho_euleriano()
+        if ciclo[0] == ciclo[-1]:
+            ciclo.reverse()
+            return ciclo
+        else:
+            return "Nenhum caminho euleriano possivel"
