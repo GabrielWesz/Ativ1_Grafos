51ffdc08ef4ffe944b7d958007b594ca2dfd2a67
Matheus Eyng de França <matheuseyngsf@gmail.com>
2020-10-29 09:49:27 -0300

    Adiciona arquivo usado para testes ex 1

---
+++ teste_conexos.py
@@ -0,0 +1,13 @@
+from grafo import Grafo
+from conexos import Conexos
+from kruskal import Kruskal
+
+arq = "conexos.txt"
+g1 = Grafo(arq)
+g1.le_grafo()
+
+conexos = Conexos(g1)
+conexos.cacula_conexos()
+
+#kruskal = Kruskal(g1)
+#kruskal.arvore_minima()
