8eec0b85986767161623429f84e21c6a07d9c7d2
Matheus Eyng de França <matheuseyngsf@gmail.com>
2020-10-29 09:50:48 -0300

    Adiciona exercicio 3 sem formatacao de saida

--- teste_conexos.py
+++ teste_conexos.py
@@ -2,12 +2,12 @@ from grafo import Grafo
 from conexos import Conexos
 from kruskal import Kruskal
 
-arq = "conexos.txt"
+arq = "kruskal.txt"
 g1 = Grafo(arq)
 g1.le_grafo()
 
-conexos = Conexos(g1)
-conexos.cacula_conexos()
+#conexos = Conexos(g1)
+#conexos.cacula_conexos()
 
-#kruskal = Kruskal(g1)
-#kruskal.arvore_minima()
+kruskal = Kruskal(g1)
+kruskal.arvore_minima()
